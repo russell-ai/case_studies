16da42c89a17e214a5bf7e7d4aec95f8b233241f
resulcaliskan <resulcaliskan@yahoo.com>
2020-10-10 23:41:21 +0300

    All about ML projects.

--- case_study_06_model_selection/k_fold_cross_validation.py
+++ case_study_06_model_selection/k_fold_cross_validation.py
@@ -1,4 +1,4 @@
-"""Kernel SVM"""
+"""k-Fold Cross Validation"""
 
 #%% Importing the libraries
 import numpy as np
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #%% Importing the dataset
-dataset = pd.read_csv('case_study_03_classification\Social_Network_Ads.csv')
+dataset = pd.read_csv('case_study_06_model_selection\Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 #%% Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 4)
 
 #%% Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -22,20 +22,19 @@ X_test = sc.transform(X_test)
 
 #%% Training the Kernel SVM model on the Training set
 from sklearn.svm import SVC
-classifier = SVC(kernel="rbf", random_state=4)
-classifier.fit(X_train,y_train)
-
-
-#%% Predicting a new result
-classifier.predict([[30,90000]])
-
-#%% Predicting the Test set results
-y_pred = classifier.predict(X_test)
-result = pd.DataFrame({"True Value":y_test,"Predict Value":y_pred})
-# print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+classifier = SVC(kernel = 'rbf', random_state = 4)
+classifier.fit(X_train, y_train)
 
 #%% Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
+y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
-accuracy_score(y_test,y_pred)
+print(cm)
+score = accuracy_score(y_test, y_pred)
+print(score)
 
+#%% Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print(f"Accuracy :{accuracies.mean()*100:.2f}")
+print(f"Standart Deviation: {accuracies.std()*100:.2f}")
