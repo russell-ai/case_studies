e4a0118d43845659a6332044bbb3a17d136a8bad
resulcaliskan <resulcaliskan@yahoo.com>
2020-10-11 08:40:10 +0300

    All about ML projects. K_Means.

---
+++ case_study_04_clustering/k_means.py
@@ -0,0 +1,17 @@
+"""K-Means Clustering"""
+
+#%%Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+
+# Using the elbow method to find the optimal number of clusters
+
+
+# Training the K-Means model on the dataset
+
+
+# Visualising the clusters
