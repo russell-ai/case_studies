49ef3a0cb8825369bf885732957cd16b65bb7f39
resulcaliskan <resulcaliskan@yahoo.com>
2020-10-11 09:13:43 +0300

    All about ML projects. PCA.

---
+++ case_study_05_dimensionality_reduction/principal_companent_analysis.py
@@ -0,0 +1,21 @@
+# Principal Component Analysis (PCA)
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+
+# Splitting the dataset into the Training set and Test set
+
+# Feature Scaling
+
+
+# Applying PCA
+
+# Training the Logistic Regression model on the Training set
+
+
+# Making the Confusion Matrix
