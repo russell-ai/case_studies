91b3e7b1f6471f709fd0585f27bac938e39d5f55
resulcaliskan <resulcaliskan@yahoo.com>
2020-10-11 08:33:56 +0300

    All about ML projects.Grid Search.

--- case_study_06_model_selection/grid_search.py
+++ case_study_06_model_selection/grid_search.py
@@ -43,16 +43,20 @@ print(f"Standart Deviation: {accuracies.std()*100:.2f}")
 
 #%% Applying Grid Search to find the best model and the best parameters
 from sklearn.model_selection import GridSearchCV
+
 parameters = [{"C":[0.5,0.75,1],"kernel":["linear"]},
               {"C":[0.5,0.75,1],"kernel":["rbf"],"gamma":[0.6,0.8,1]},
               {"C":[0.5,0.75,1],"kernel":["poly"],"degree":[2,3],"gamma":[0.6,0.8,1]},
               {"C":[0.5,0.75,1],"kernel":["sigmoid"],"gamma":[0.6,0.8,1]}]
+
 gird_search = GridSearchCV(estimator=classifier,
                            param_grid=parameters,
                            scoring="accuracy",
                            cv=10,
                            n_jobs=-1)
+
 gird_search.fit(X_train,y_train)
+
 print("Best Score:",gird_search.best_score_)
 print("Best Parameters:",gird_search.best_params_)
 
