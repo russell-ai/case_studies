d32378d773ce4c2dec5e730b766d669476eeaa2e
resulcaliskan <resulcaliskan@yahoo.com>
2020-10-13 13:40:46 +0300

    All about ML for case studies project. Clustering.

--- case_study_04_clustering/k_means.py
+++ case_study_04_clustering/k_means.py
@@ -5,13 +5,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#%% Importing the dataset
+df = pd.read_csv("datasets/Mall_Customers.csv")
+df.head(3)
 
+#%% Identifying independent training variables
 
-# Using the elbow method to find the optimal number of clusters
+X = df.iloc[:,[3,4]].values
+X[:2] # first two row
 
+#%% Using the elbow method to find the optimal number of clusters
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans_cluster = KMeans(n_clusters=i, init="k-means++", random_state=0)
+    kmeans_cluster.fit(X)
+    wcss.append(kmeans_cluster.inertia_)
 
-# Training the K-Means model on the dataset
+plt.plot(range(1, 11), wcss)
+plt.title("The Elbow Method")
+plt.xlabel("Number of Clusters")
+plt.ylabel("WCSS")
+plt.show()
 
+#%% Training the K-Means model on the dataset
+kmeans_cluster = KMeans(n_clusters=5, init="k-means++", random_state=0)
+y_pred = kmeans_cluster.fit_predict(X)
 
-# Visualising the clusters
+#%% Visualising the clusters
+
+plt.scatter(X[y_pred == 0, 0], X[y_pred == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
+plt.scatter(X[y_pred == 1, 0], X[y_pred == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
+plt.scatter(X[y_pred == 2, 0], X[y_pred == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
+plt.scatter(X[y_pred == 3, 0], X[y_pred == 3, 1], s = 100, c = 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_pred == 4, 0], X[y_pred == 4, 1], s = 100, c = 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans_cluster.cluster_centers_[:, 0], kmeans_cluster.cluster_centers_[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
